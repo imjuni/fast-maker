3e1edac66c974484b7b8ae4edef50a8f78977e0f
ByungJoon Lee <imjuni@gmail.com>
2023-03-05 17:35:21 +0900

    Upgrade outdated package
    * upgrade outdated package
    
    * deps
      * dev
        * change @types/yargs: 17.0.19 > 17.0.22
        * change @typescript-eslint/eslint-plugin: 5.53.0 > 5.54.0
        * change @typescript-eslint/parser: 5.53.0 > 5.54.0
        * change eslint: 8.34.0 > 8.35.0
        * change eslint-plugin-jsdoc: 40.0.0 > 40.0.1
        * change rimraf: 4.1.2 > 4.3.0
        * change rollup: 3.9.1 > 3.18.0
      * prod
        * change fastify: 4.13.0 > 4.14.0
        * change mathjs: 11.5.1 > 11.6.0
        * change pino-pretty: 9.3.0 > 9.4.0
        * change type-fest: 3.6.0 > 3.6.1
        * change yargs: 17.6.2 > 17.7.1

--- src/configs/isValidConfig.ts
+++ src/configs/isValidConfig.ts
@@ -1,11 +1,18 @@
 import getResolvePath from '#configs/getResolvePath';
-import type { TRouteBaseOption } from '#configs/interfaces/TRouteOption';
-import type { TWatchBaseOption } from '#configs/interfaces/TWatchOption';
-import type { Arguments } from 'yargs';
 
-export default function isValidConfig(argv: Arguments<TRouteBaseOption | TWatchBaseOption>) {
+function getConfigValue(value: unknown) {
+  if (typeof value === 'string') {
+    return value;
+  }
+
+  // this literal raise error on existSync function
+  return undefined;
+}
+
+export default function isValidConfig(argv: { [argName: string]: unknown; _: (string | number)[]; $0: string }) {
   // check project file exits
-  const { project, handler } = argv;
+  const handler = getConfigValue(argv.handler) ?? '';
+  const project = getConfigValue(argv.project) ?? '';
 
   if (getResolvePath(handler) === false) {
     throw new Error(`Invalid handler path: ${handler}`);
