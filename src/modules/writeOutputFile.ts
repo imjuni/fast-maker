ad5cd1d8339a5acc40a4e952a9da813ee062504d
ByungJoon Lee <imjuni@gmail.com>
2023-03-05 17:56:56 +0900

    Fix writeOutputFile
    * fix writeOutputFile: skip stringify process that in case of string, buffer pass

--- src/modules/writeOutputFile.ts
+++ src/modules/writeOutputFile.ts
@@ -3,12 +3,25 @@ import fs from 'fs';
 import { exists } from 'my-node-fp';
 import path from 'path';
 
+function getBuffer(data: unknown) {
+  if (typeof data === 'string') {
+    return data;
+  }
+
+  if (data instanceof Buffer) {
+    return data;
+  }
+
+  return fastSafeStringify(data);
+}
+
 export default async function writeOutputFile<T = string>(filePath: string, data: T) {
   if (await exists(filePath)) {
     await fs.promises.writeFile(filePath, fastSafeStringify(data));
   }
 
   const dirPath = path.dirname(filePath);
+
   await fs.promises.mkdir(dirPath, { recursive: true });
-  await fs.promises.writeFile(filePath, fastSafeStringify(data));
+  await fs.promises.writeFile(filePath, getBuffer(data));
 }
