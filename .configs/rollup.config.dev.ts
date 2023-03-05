8ef51af890bc72ea118edf41c91e650157615294
ByungJoon Lee <imjuni@gmail.com>
2023-03-05 17:13:19 +0900

    Change bundle architecture
    * change bundle architecture
      * apply `@rollup/plugin-typescript`, `@swc/core`
      * apply .d.ts bundle using `rollup-plugin-dts`
    * change cli bundle configuration
      * cli script don't compress(without terser)

--- .configs/rollup.config.dev.ts
+++ .configs/rollup.config.dev.ts
@@ -1,8 +1,21 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { readFileSync } from 'fs';
+import path from 'path';
 import readPackage from 'read-pkg';
-import ts from 'rollup-plugin-ts';
+import dts from 'rollup-plugin-dts';
+import { swc } from 'rollup-plugin-swc3';
+import ts from 'typescript';
 
 const pkg = readPackage.sync();
+
+const resolveOnly = (module) => {
+  return (
+    pkg?.dependencies?.[module] != null &&
+    pkg?.devDependencies?.[module] != null &&
+    pkg?.peerDependencies?.[module] != null
+  );
+};
 
 export default [
   {
@@ -11,14 +24,17 @@ export default [
       dir: 'dist',
       format: 'cjs',
       banner: '#!/usr/bin/env node',
+      sourcemap: true,
     },
     plugins: [
-      nodeResolve({
-        resolveOnly: (module) => {
-          return pkg?.dependencies?.[module] == null && pkg?.devDependencies?.[module] == null;
+      nodeResolve({ resolveOnly }),
+      typescript({
+        tsconfig: 'tsconfig.prod.json',
+        compilerOptions: {
+          sourceMap: true,
         },
       }),
-      ts({ tsconfig: 'tsconfig.json' }),
+      swc(),
     ],
   },
   {
@@ -26,22 +42,48 @@ export default [
     output: [
       {
         format: 'cjs',
-        file: 'dist/cjs/fast-maker.js',
+        file: 'dist/cjs/index.js',
         sourcemap: true,
       },
       {
         format: 'esm',
-        file: 'dist/esm/fast-maker.js',
+        file: 'dist/esm/index.js',
         sourcemap: true,
       },
     ],
+
     plugins: [
-      nodeResolve({
-        resolveOnly: (module) => {
-          return pkg?.dependencies?.[module] == null && pkg?.devDependencies?.[module] == null;
+      nodeResolve({ resolveOnly }),
+      typescript({
+        tsconfig: 'tsconfig.prod.json',
+        compilerOptions: {
+          sourceMap: true,
         },
       }),
-      ts({ tsconfig: 'tsconfig.json' }),
+      swc(),
+    ],
+  },
+  {
+    input: 'dist/esm/src/index.d.ts',
+    output: [
+      {
+        file: 'dist/cjs/index.d.ts',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/esm/index.d.ts',
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      nodeResolve({ resolveOnly }),
+      dts({
+        compilerOptions: {
+          baseUrl: 'dist/cjs',
+          paths: ts.readConfigFile(path.resolve('./tsconfig.json'), (p) => readFileSync(p, 'utf8')).config
+            .compilerOptions.paths,
+        },
+      }),
     ],
   },
 ];
